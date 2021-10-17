cc0c17886729bf9c39d669a5a05fdabaa2ee1adb
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-10-17 17:54:19 +0200

    Add logout, avatar

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
-router.get('/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/user/no-permission" }),
   (req, res) => {
-    res.redirect('/user/logged');
+    res.redirect("/user/logged");
   }
 );
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+const isLogged = (req, res, next) => {
+  if (req.user == undefined) {
+    res.redirect("/user/no-permission");
+    console.log(req.user);
+  } else {
+    next();
+  }
+};
+
+router.get("/logged", isLogged, (req, res) => {
+  console.log("user", req.user);
+  res.render("logged", {
+    name: req.user.displayName,
+    avatar: req.user.photos[0].value,
+  });
+});
+router.get("/profile", isLogged, (req, res) => {
+  res.send("your profile");
+});
+router.get("/profile/settings", isLogged, (req, res) => {
+  res.send("your profile settings");
 });
 
-router.get('/no-permission', (req, res) => {
-  res.render('noPermission');
+router.get("/no-permission", (req, res) => {
+  res.render("noPermission");
 });
-
 
 module.exports = router;
